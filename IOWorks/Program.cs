82734d3f25736514fffc985456df4d80a5a53081
Toyin96 <onagoruwam@gmail.com>
2021-11-11 07:58:58 +0100

    added the ioworks assignment

---
+++ IOWorks/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using IOWorks.models;
+
+namespace IOWorks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> students = new Dictionary<string, int>();
+            students.Add("toyin", 64);
+            students.Add("seun", 20);
+            students.Add("bayo", 50);
+            students.Add("dan", 23);
+            students.Add("gbenga", 65);
+            students.Add("joy", 74);
+            students.Add("toyib", 81);
+            students.Add("amaka", 49);
+            students.Add("tosin", 20);
+            students.Add("ebuka", 28);
+            students.Add("ifeanyi", 15);
+            students.Add("lanre", 69);
+            students.Add("chidi", 45);
+            students.Add("hamdan", 49);
+            students.Add("chibuzo", 10);
+            students.Add("kunle", 64);
+            students.Add("tunde", 46);
+            students.Add("chinenye", 31);
+            students.Add("chinelo", 39);
+            students.Add("ujunwa", 50);
+
+            IOClient ioClient = new IOClient();
+            var path = ioClient.ComputeScores(students);
+            ioClient.UpgradeScores(path);
+
+            ioClient.LogFile();
+        }
+    }
+}
---
+++ IOWorks/models/IOClient.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+
+namespace IOWorks.models
+{
+    public class IOClient
+    {
+        #region updateStudentsScores
+        public void UpgradeScores(string path)
+        {
+            var newPath = "updatedlist.txt";
+
+            if (!File.Exists(newPath))
+            {
+                File.Create(newPath);
+            }
+
+            foreach (var student in File.ReadAllLines(path))
+            {
+                string[] eachStudent = student.Split("-");
+                int score = int.Parse(eachStudent[1]);
+
+                if (score > 50)
+                {
+                    score += 5;
+                }
+                else
+                {
+                    score += 10;
+                }
+
+                var finalStudent = $"{eachStudent[0]} -     {score}";
+
+                File.AppendAllLines(newPath, new string[] { finalStudent });
+            }
+            Console.WriteLine("Students scores has ben successfully updated");
+        }
+
+        public string ComputeScores(Dictionary<string, int> studentsInfo)
+        {
+            var path = "studentgrades.txt";
+
+            if (!File.Exists(path))
+            {
+                File.Create(path);
+            }
+
+            string[] studentsGrades = new string[20];
+            var studentCatalog = "";
+
+            foreach (var student in studentsInfo)
+            {
+                studentCatalog = $"{student.Key.ToUpper()}-{student.Value}";
+                File.AppendAllLines(path, new string[] { studentCatalog });
+            }
+
+            return path;
+        }
+        #endregion
+
+        #region TimeUpdate
+
+        public void LogFile()
+        {
+            var path = "timelog.txt";
+            var myStack = new Stack<string>();
+
+            //if (!File.Exists(path))
+            //{
+            //    File.Create(path);
+            //}
+
+            var counter = 0;
+            while (counter < 10)
+            {
+                Thread.Sleep(10000);
+
+                var time = DateTime.UtcNow;
+                myStack.Push($"The time is {time.ToString()}");
+
+                counter++;
+            }
+
+            try
+            {
+                addToFile(myStack, path);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+        public void addToFile(Stack<string> arg, string path)
+        {
+            while (arg.Count > 0)
+            {
+                File.AppendAllLines(path, new string[] { arg.Pop().ToString() });
+            }
+
+            Console.WriteLine("Finished logging the time data");
+        }
+        #endregion
+    }
+}
